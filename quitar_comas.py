28870e926b4ff1cd35d58fc10d98196b8b9c923b
mcortesru <114393826+mcortesru@users.noreply.github.com>
2024-01-10 23:01:35 +0100

    Add files via upload

---
+++ quitar_comas.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import os
+
+# Ruta al directorio que contiene los archivos Excel
+directorio_excel = './'
+
+# Ruta al directorio donde se guardarán los archivos CSV
+directorio_csv = './csv_output'
+
+# Obtener la lista de archivos Excel en el directorio
+archivos_excel = [archivo for archivo in os.listdir(directorio_excel) if archivo.endswith('.xlsx')]
+
+# Iterar sobre cada archivo Excel
+for archivo_excel in archivos_excel:
+    # Leer el archivo Excel
+    print("Procesando ", archivo_excel)
+    ruta_excel = os.path.join(directorio_excel, archivo_excel)
+    df_excel = pd.read_excel(ruta_excel, sheet_name=None)
+
+    # Iterar sobre cada hoja del archivo Excel
+    for hoja, datos in df_excel.items():
+        print("         Procesando hoja ", hoja)
+        # Reemplazar ',' y ';' por un número (por ejemplo, 0)
+        datos = datos.replace({',': 0, ';': 0})
+
+        # Crear la ruta para guardar el archivo CSV
+        nombre_csv = f"{archivo_excel}_{hoja}.csv"
+        ruta_csv = os.path.join(directorio_csv, nombre_csv)
+
+        # Guardar los datos en un archivo CSV
+        datos.to_csv(ruta_csv, index=False)
+
+print("Proceso completado. Archivos CSV guardados en:", directorio_csv)
